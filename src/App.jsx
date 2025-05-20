2e96d7c851d1a49f9980f264c1183bb4e3af63ee
EnricoRP <email@enricorp.com>
2025-05-20 15:53:29 +0700

    penambahan komponen Heading yang dapat di gunakan kembali

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react' 
 import ButtonGradient from './assets/svg/ButtonGradient'
 import Header from './components/Header'
+import Benefits from './components/Benefits'
 import Hero from './components/Hero'
 const App = () => {
   return (
@@ -8,6 +9,7 @@ const App = () => {
       <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
         <Header />
         <Hero />
+        <Benefits />
       </div>
       <ButtonGradient />
     </>
