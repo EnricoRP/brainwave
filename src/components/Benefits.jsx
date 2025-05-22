241b7f25d9b0b212cc5552586691b9b5c848f07d
EnricoRP <email@enricorp.com>
2025-05-22 14:36:16 +0700

    penambahan komponen benefit

--- src/components/Benefits.jsx
+++ src/components/Benefits.jsx
@@ -1,15 +1,45 @@
 import React from 'react'
 import Section from './Section'
 import Heading from './Heading'
+import { benefits } from '../constans'
+import Arrow from '../assets/svg/Arrow'
 
 const Benefits = () => {
-  return (
-    <Section id="features">
-        <div className='container relative z-2'>
-            <Heading className="md:max-w-md lg:max-2-2xl" title="Chat Smarter, Not Harder with Brainwave"/>
-        </div>
-    </Section>
-  )
+    return (
+        <Section id="features">
+            <div className='container relative z-2'>
+                <Heading className="md:max-w-md lg:max-2-2xl" title="Chat Smarter, Not Harder with Brainwave" />
+                <div className='flex flex-wrap gap-10 mb-10'>
+                    {benefits.map((item) => (
+                        <div
+                            className='block relative p-0.5 bg-no-repeat bg-[length:100%_100%] md:max-w-[24rem]'
+                            style={{ backgroundImage: `url(${item.backgroundUrl})` }}
+                            key={item.id}
+                        >
+                            <div className='relative z-2 flex flex-col min-h-[22rem] p-[2.4rem] pointer-events-none'>
+                                <h5 className='h2 mb-5'>
+                                    {item.title}
+                                </h5>
+                                <p className="body-2 mb-6 text-n-3">
+                                    {item.text}
+                                </p>
+                                <div className="flex mt-auto items-center">
+                                    <img 
+                                        src={item.iconUrl}
+                                        width={48}
+                                        height={40}
+                                        alt={item.title}
+                                    />
+                                    <p className='ml-auto font-code text-xs font-bold text-n-1 uppercase tracking-wider'>Explore more</p>
+                                    <Arrow />
+                                </div>
+                            </div>
+                        </div>
+                    ))}
+                </div>
+            </div>
+        </Section>
+    )
 }
 
 export default Benefits
